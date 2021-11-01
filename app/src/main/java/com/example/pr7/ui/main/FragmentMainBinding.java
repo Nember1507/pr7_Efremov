382b178ae4aa65e7b1b67c24f520fc02fc448f76
Nember <efremovzhenia2104@gmail.com>
2021-11-01 05:41:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pr7/ui/main/FragmentMainBinding.java
@@ -0,0 +1,18 @@
+package com.example.pr7.ui.main;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentMainBinding {
+    public TextView sectionLabel;
+
+    public static void inflate(LayoutInflater inflater, ViewGroup container, boolean b) {
+
+    }
+    public View getRoot() {
+
+        return null;
+    }
+}
